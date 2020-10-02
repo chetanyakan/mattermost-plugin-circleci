5631c130152313ce4a1f147cf746fcab76d91819
Harshil Sharma <harshilsharma63@gmail.com>
2020-10-02 20:58:28 +0530

    fixes

--- server/command/circleci.go
+++ server/command/circleci.go
@@ -153,7 +153,7 @@ var commandUnsubscribe = &command{
 var commandListSubscriptions = &command{
 	Execute: executeListSubscriptions,
 	AutocompleteData: &model.AutocompleteData{
-		Trigger:     "list subscriptions",
+		Trigger:     "list-subscriptions",
 		HelpText:    "Get list of CircleCI notifications subscribed in the current channel",
 		Arguments:   nil,
 		SubCommands: nil,
@@ -581,9 +581,9 @@ var CircleCICommandHandler = Handler{
 		"disconnect":         commandDisconnect.Execute,
 		"subscribe":          commandSubscribe.Execute,
 		"unsubscribe":        commandUnsubscribe.Execute,
-		"list/subscriptions": commandListSubscriptions.Execute,
+		"list-subscriptions": commandListSubscriptions.Execute,
 		"build":              commandBuild.Execute,
-		"recent/builds":      commandRecentBuilds.Execute,
+		"recent-builds":      commandRecentBuilds.Execute,
 		// These can be used later when adding Github and Bitbucket on-premise support
 		//"add/vcs":            commandAddVCS.Execute,
 		//"delete/vcs":         commandDeleteVCS.Execute,
@@ -944,7 +944,7 @@ func executeBuild(ctx *model.CommandArgs, args ...string) (*model.CommandRespons
 		attachmentFields,
 		&model.SlackAttachmentField{
 			Title: "Build Number",
-			Value: build.Number,
+			Value: fmt.Sprintf("%d", build.Number),
 			Short: true,
 		},
 		&model.SlackAttachmentField{
@@ -964,12 +964,22 @@ func executeBuild(ctx *model.CommandArgs, args ...string) (*model.CommandRespons
 	attachment.Text = fmt.Sprintf("CircleCI build %d initiated successfully.", build.Number)
 	attachment.Fields = attachmentFields
 
-	return &model.CommandResponse{
-		Username:    config.BotDisplayName,
-		IconURL:     config.BotIconURL,
-		Type:        model.COMMAND_RESPONSE_TYPE_IN_CHANNEL,
-		Attachments: []*model.SlackAttachment{attachment},
-	}, nil
+	post := &model.Post{
+		UserId:    config.BotUserID,
+		ChannelId: ctx.ChannelId,
+	}
+
+	model.ParseSlackAttachment(post, []*model.SlackAttachment{attachment})
+
+	if _, err := config.Mattermost.CreatePost(post); err != nil {
+		config.Mattermost.LogError(fmt.Sprintf(
+			"Failed to create post for triggered build. Projet: %s, err: %s",
+			vcs.Type+"/"+org+"/"+repo,
+			err.Error()),
+		)
+	}
+
+	return &model.CommandResponse{}, nil
 }
 
 // func executeAddVCS(context *model.CommandArgs, args ...string) (*model.CommandResponse, *model.AppError) {
